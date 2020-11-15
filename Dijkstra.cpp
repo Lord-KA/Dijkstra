96cd48d174611e81f62c5b5fe4a35869370a7718
Lord <lord.nemo@protonmail.com>
2020-11-15 14:55:01 -0500

    small fix

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -148,6 +148,7 @@ int main()
     graph.Dijkstra(start);
     //std::cout << graph.Matrix << std::endl;
     //std::cout << graph.Distance << std::endl;
+    //
     std::cout << graph.restoreRoute(start, finish) << std::endl;
     return 0;
 }
